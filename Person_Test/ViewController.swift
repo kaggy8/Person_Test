d117e426c300bb0370f6708856e7d00321ce1cd5
Stanislav <kaggy8@ya.ru>
2022-11-06 01:12:23 +0300

    Initial Commit

---
+++ Person_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Person_Test
+//
+//  Created by Stanislav Demyanov on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
